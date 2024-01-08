86ab79dd939eb6546b4d911328f99c5857757dc3
Colorless9 <2514693500@qq.com>
2024-01-08 16:01:40 +0800

    feat:新增了用于消除全满的行的map_clearer模块

---
+++ tetris.srcs/map_clearer.sv
@@ -0,0 +1,18 @@
+module map_clearer(
+    input clk,
+    input [19:0][9:0]map,
+    output [19:0][9:0]next_map
+);
+    reg [19:0][9:0]g;
+    reg [5:0]t,i;
+    always @(posedge clk) begin
+        t=19;
+        for(i=19; i>=0;i--)begin
+            while(t>=0 && map[t]==10'b1111111111)t=t-1;
+            if(t<0)g[i] <= 10'b0;
+            else g[i] <= map[t];
+        end    
+    end
+    
+    assign next_map=g;
+endmodule
