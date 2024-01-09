a5c67e04264304300a31de44c1f8d22fff37ae1e
Colorless9 <2514693500@qq.com>
2024-01-09 13:26:01 +0800

    feat:新增对tetrominoes_factory的仿真文件以及仿真结果

---
+++ tetris.srcs/sim_1/new/tetrominoes_factory_tb.sv
@@ -0,0 +1,30 @@
+`include "types.svh"
+module tetrominoes_factory_tb();
+reg clk;
+tetro_type_t tetro_type;
+shortint delta_rows[2:0];
+shortint delta_cols[2:0];
+tetro_type_t next_type;
+
+tetrominoes_factory tetrominoes_factory_inst(
+    .clk(clk),.tetro_type(tetro_type),.delta_rows(delta_rows),.next_type(next_type)
+);
+
+initial begin
+    tetro_type_t res;
+    res=0;clk=0;
+    repeat(19)begin
+        tetro_type = res;
+        res=res + 1;
+        clk=!clk;
+        #10;
+        clk=!clk;
+        #10;
+        clk=!clk;
+        #10;
+        clk=!clk;
+        #10;
+        
+    end
+end
+endmodule
