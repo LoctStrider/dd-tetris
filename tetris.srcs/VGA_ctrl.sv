4171beb5aaf9db10b35d1b4c4944632248bd211d
Colorless9 <2514693500@qq.com>
2024-01-09 18:21:55 +0800

    fix:解决了横向的长条块不能在右上角小框完全显示的问题

--- tetris.srcs/VGA_ctrl.sv
+++ tetris.srcs/VGA_ctrl.sv
@@ -1,5 +1,5 @@
 `include "types.svh"
-//用于对于给定的map，调用VGA进行整张图的输出。
+//用于对于给定的map，调用VGA进行整张图的输出�?
 module VGA_ctrl(
 
     input vga_clk,
@@ -43,8 +43,8 @@ module VGA_ctrl(
     shortint id_col = nowh / 20;
     reg[11:0]color_now;
     wire [11:0] color=color_now;
-    //注意，这个编号和map的下标不同。map之中存的是边框之外的格子的信息，而加上边框之后向右下偏移了一位，即id_row=x,id_col=y对应map[x+1][y+1].
-    //同理，对于右边用来显示下一个块的3*4的格子阵，其左上角对应的id_row=1,id_col=12.
+    //注意，这个编号和map的下标不同�?�map之中存的是边框之外的格子的信息，而加上边框之后向右下偏移了一位，即id_row=x,id_col=y对应map[x+1][y+1].
+    //同理，对于右边用来显示下�?个块�?3*4的格子阵，其左上角对应的id_row=1,id_col=12.
     shortint active_row_0 = active_center_row + active_delta_rows[0];
     shortint active_col_0 = active_center_col + active_delta_cols[0];
     shortint active_row_1 = active_center_row + active_delta_rows[1];
@@ -61,11 +61,11 @@ module VGA_ctrl(
     
     always @ (posedge vga_clk) begin
         if(addr_valid)S <= 0;
-        else if(id_row==0&&id_col<=15)S <= 3;//第一行灰色框
-        else if(id_row==5&&id_col>=11&&id_col<=15)S <= 3;
-        else if(id_row==21&&id_col<=11)S <= 3;//最后一行灰色框
+        else if(id_row==0&&id_col<=16)S <= 3;//第一行灰色框
+        else if(id_row==5&&id_col>=11&&id_col<=16)S <= 3;
+        else if(id_row==21&&id_col<=11)S <= 3;//�?后一行灰色框
         else if(id_row<=21&&(id_col==0||id_col==11))S<=3;//两列的灰色框
-        else if(id_row<=5&&id_col==15)S <= 3;//灰色小框
+        else if(id_row<=5&&id_col==16)S <= 3;//灰色小框
         else if(id_row<=20 && id_col<=10)begin
             if(active_exist&& (id_row-1==active_center_row) && (id_col-1==active_center_col) ) S <= 2;//正在掉落的块
             else if(active_exist&& (id_row-1==active_row_0) && (id_col-1==active_col_0) ) S <= 2;
@@ -75,7 +75,7 @@ module VGA_ctrl(
             else S<=1;
         end
         else begin
-            if( (id_row-1==next_center_row) && (id_col-1==next_center_col) ) S <= 2;//旁边显示的下一个块
+            if( (id_row-1==next_center_row) && (id_col-1==next_center_col) ) S <= 2;//旁边显示的下�?个块
             else if( (id_row-1==next_row_0) && (id_col-1==next_col_0) ) S <= 2;
             else if( (id_row-1==next_row_1) && (id_col-1==next_col_1) ) S <= 2;
             else if( (id_row-1==next_row_2) && (id_col-1==next_col_2) ) S <= 2;
@@ -92,7 +92,7 @@ endmodule
 
 //if(id_row==0&&id_col<=17)S <= 1;//第一行灰色框
 //        else if(id_row==5&&id_col>=13&&id_col<=17)S <=1;
-//        else if(id_row==23&&id_col<=13)S<=1;//最后一行灰色框
+//        else if(id_row==23&&id_col<=13)S<=1;//�?后一行灰色框
 //        else if(id_row<=23&&(id_col==0||id_col==13))S<=1;//两列的灰色框
 //        else if(id_row<=5&&id_col==17)S <= 1;//灰色小框
 //        else S <= 0;
