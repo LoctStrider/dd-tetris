12569c8fc072cd616bfe5ca2dd220a7b4dfeb3fa
LoctStrider <lkc314@qq.com>
2024-01-09 10:19:25 +0800

    fix: map_clearer 无符号比较无法正常实现功能

--- tetris.srcs/map_clearer.sv
+++ tetris.srcs/map_clearer.sv
@@ -4,7 +4,7 @@ module map_clearer(
     output [19:0][9:0]next_map
 );
     reg [19:0][9:0]g;
-    reg [5:0]t,i;
+    reg signed [5:0]t,i;
     always @(posedge clk) begin
         t=19;
         for(i=19; i>=0;i--)begin
